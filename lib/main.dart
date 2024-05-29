e93559f6b0e16e8587cd0206834d2a7f78b6148d
Vichea <104241903+CheaTXT@users.noreply.github.com>
2024-05-30 05:10:03 +0700

    finish TP02

--- lib/main.dart
+++ lib/main.dart
@@ -10,31 +10,96 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Display Image from Network'),
+          title: const Center(
+            child: Text('Flutter layout demo')
+          )
         ),
-        body: Center(
-          child: Image.network(
-            'https://images.pexels.com/photos/23169741/pexels-photo-23169741/free-photo-of-a-green-plant-growing-on-a-wall.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-            // Optionally, add a placeholder while the image loads
-            loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
-              if (loadingProgress == null) {
-                return child;
-              } else {
-                return Center(
-                  child: CircularProgressIndicator(
-                    value: loadingProgress.expectedTotalBytes != null
-                        ? loadingProgress.cumulativeBytesLoaded / (loadingProgress.expectedTotalBytes ?? 1)
-                        : null,
+
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+
+            // Row1
+            Image.network(
+              'https://preview.redd.it/lhh5tq0qjjr31.jpg?width=1080&crop=smart&auto=webp&s=8b6a74a5e71561e14636476d7e96aea60316e358'
+            ),
+
+            // Row2
+            const Padding(
+              padding: EdgeInsets.all(30.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Oeschinen Lake Campground', style: TextStyle(fontWeight: FontWeight.bold,fontSize: 16)),
+                      SizedBox(height: 10,),
+                      Text('Oeschinen Lake Campground',style: TextStyle(fontSize: 12, color: Colors.grey)),
+                    ],
                   ),
-                );
-              }
-            },
-            // Optionally, add error handling
-            errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace) {
-              return Text('Failed to load image');
-            },
-          ),
+                  Column(
+                    children: [
+                      Row(
+                        children: [
+                          Icon(Icons.star, color: Color.fromARGB(255, 230, 80, 11),),
+                          Padding(
+                            padding: EdgeInsets.only(left: 4.0),
+                            child: Text('41'),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+
+            // Row3
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 60.0, vertical: 0.0),
+              child: IconTheme(
+                data: IconThemeData(color: Color.fromARGB(255, 73, 49, 131)),
+                child: DefaultTextStyle(
+                  style: TextStyle(color: Color.fromARGB(255, 73, 49, 131)),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        children: [
+                          Icon(Icons.call),
+                          Text('CALL'),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Icon(Icons.arrow_upward),
+                          Text('data'),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Icon(Icons.share),
+                          Text('data'),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+
+            //Row4
+            const Padding(
+              padding: EdgeInsets.all(30.0), 
+              child: 
+                Text(
+                  'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes. A gondola ride from Kandersteg, followed by a half-hour walk through pastures and pine forest, leads you to the lake, which warms to 20 degrees Celsius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run.'
+                )
+            )
+          ],
         ),
+
       ),
     );
   }
